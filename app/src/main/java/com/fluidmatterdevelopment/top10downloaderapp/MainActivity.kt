bdc10b890101a2bd99d8d9832c1705fe7f8597be
Jordan <jcrerar@gmail.com>
2019-10-15 20:32:47 -0400

    first commit

---
+++ app/src/main/java/com/fluidmatterdevelopment/top10downloaderapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fluidmatterdevelopment.top10downloaderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
