5fdae79d22d20a9ba9c9471e64d7ca598b800b55
Jordan <jcrerar@gmail.com>
2019-10-21 03:34:17 -0400

    Pre custon ListView Adapter commit

--- app/src/main/java/com/fluidmatterdevelopment/top10downloaderapp/MainActivity.kt
+++ app/src/main/java/com/fluidmatterdevelopment/top10downloaderapp/MainActivity.kt
@@ -1,12 +1,87 @@
 package com.fluidmatterdevelopment.top10downloaderapp
 
+import android.content.Context
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.AbsListView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
+import kotlin.properties.Delegates
+
+class FeedEntry {
+    var name = ""
+    var artist = ""
+    var releaseDate = ""
+    var summary = ""
+    var imageUrl = ""
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $releaseDate
+            imageUrl = $imageUrl
+        """.trimIndent()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private val downLoadData by lazy { DownloadData(this, xmlListView)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate: Called")
+        downLoadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml")
+        Log.d(TAG, "onCreate: Done")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        downLoadData.cancel(true)
+    }
+
+    companion object {
+        private class DownloadData(context: Context, listView: ListView) : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+
+            var parentContext: Context by Delegates.notNull()
+            var parentListView: ListView by Delegates.notNull()
+
+            init{
+                parentContext = context
+                parentListView = listView
+            }
+
+
+
+            override fun onPostExecute(result: String) {
+                super.onPostExecute(result)
+//                Log.d(TAG, "onPostExecute: parameter is $result"
+                val parseApplication = ParseApplications()
+                parseApplication.parse(result)
+
+                val arrayAdapter = ArrayAdapter<FeedEntry>(parentContext, R.layout.list_item, parseApplication.applications)
+                parentListView.adapter = arrayAdapter
+            }
+
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: start with: ${url[0]}")
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error downloading")
+                }
+                return rssFeed
+            }
+
+            private fun downloadXML(urlPath: String?): String {
+                return URL(urlPath).readText()
+            }
+        }
     }
 }
